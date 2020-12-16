525022fe7b2632776d4948cc0d537d9d53acb41a
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-16 12:00:29 -0500

    Add files via upload

---
+++ countdown.rb
@@ -0,0 +1,19 @@
+
+def countdown(number)
+    number = 10
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+    number = 10
+    sleep(5)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
